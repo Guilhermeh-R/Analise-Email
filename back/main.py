f2b868789d015d59287b79095e6d3810d52576ad
guilhermeh-r <rodriguesguilerme4@gmail.com>
2025-09-15 18:18:19 -0300

    porta render

--- back/main.py
+++ back/main.py
@@ -5,11 +5,12 @@ from deep_translator import GoogleTranslator
 from controller import *
 from flask_cors import CORS
 import requests
-import asyncio
+import os
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render define a PORT
+    app.run(host="0.0.0.0", port=port)
